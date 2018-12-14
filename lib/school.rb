748cfba23f8905bdc42bf410b564d2c0889c20c1
Yukiji Nakatsuka <lunarbase@outlook.com>
2018-12-14 06:08:52 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,35 @@
 # code here!
+require 'pry'
+class School
+  
+  def initialize(name)
+    @name = name 
+    @roster = {}
+  end
+  
+  def roster 
+    @roster 
+  end
+  
+  def add_student(student, grade)
+    if @roster[grade]
+      @roster[grade] << student
+    else 
+      @roster[grade] = []
+      @roster[grade] << student
+    end
+  end 
+  
+  
+  def grade(grade)
+    @roster[grade] if @roster.keys.include?(grade)
+  end
+  
+  def sort 
+    @roster.map do |grade, students|
+      @roster[grade] = students.sort 
+    end  
+    @roster
+  end 
+  
+end
