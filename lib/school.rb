0b7138af28bccd667b7f2fc4edb36583e4b9014c
Alua Baikadamova <abaikad00@gmail.com>
2018-12-18 01:37:59 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,40 @@
 # code here!
+class School
+  def initialize(name)
+    @name=name
+    @roster = {}
+  end
+ 
+  def add_student(name, grade)
+     if @roster.has_key? grade
+    
+  
+   @roster[grade]<<name
+ else
+   @roster[grade] = []
+     @roster[grade]<<name
+ end
+  end
+ 
+ def roster
+ @roster
+ end
+ 
+ def grade(grade)
+
+  if @roster.has_key? grade
+  @roster[grade]
+end
+
+
+end
+
+def sort
+
+@roster.each do |key, value|
+  @roster[key] = value.sort_by{ |a,b| a}
+end
+
+
+end
+end
