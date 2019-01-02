c57fbe490023c1493ff28fb9b3dd11d59b37860c
Shawanda Davis <shawandadavis93@gmail.com>
2019-01-02 17:52:50 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+require "pry"
+
+class School
+  attr_accessor :roster
+  
+  def initialize(school)
+    @school = school
+    @roster = {}
+  end  
+  
+  #binding.pry
+  
+  def add_student(name, grade)
+    @roster[grade] ||= []     #What does this mean???
+    @roster[grade] << name 
+  end  
+  
+  def grade(grade)
+    @roster[grade].map {|name| name }
+  end
+  
+  def sort # this one was a bit confusing I wanted to use map
+    sort_students = {}
+    @roster.each {|grade, name| sort_students[grade] = name.sort}
+    sort_students
+  end  
+end  
